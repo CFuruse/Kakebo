ccc34b283650922d097a8fb30bcb9f42f19dfd8e
Chiharu Furuse <kutsu_rogi@yahoo.co.jp>
2015-11-16 23:17:20 +0900

    詳細検索にソート機能を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,15 +10,24 @@ module ApplicationHelper
     end
   end
 
-  def sortable(column, title = nil)
+  def sortable(column, title = nil, flag = false)
     title ||= column.titleize
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, {
-                     :sort => column,
-                     :direction => direction,
-                     :params => {:kakebo => params[:kakebo]}
-                   },
-                   {:class => css_class}
+    if flag
+      link_to title, {
+                       :sort => column,
+                       :direction => direction,
+                       :params => {:kakebo => params[:kakebo]},
+                     },
+                     {:class => css_class}
+    else
+      link_to title, {
+                       :sort => column,
+                       :direction => direction,
+                       :params => {:q => params[:q]},
+                     },
+                     {:class => css_class}
+    end
   end
 end
